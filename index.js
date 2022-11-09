418bb796acaf309edb4aac0638243b878bf1f8da
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-11-09 12:50:31 +0600

    delete review

--- index.js
+++ index.js
@@ -71,6 +71,12 @@ async function run() {
             const myReview = await cursor.toArray();
             res.send(myReview);
         });
+        app.delete('/myReview/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result)
+        })
        
 
     }
